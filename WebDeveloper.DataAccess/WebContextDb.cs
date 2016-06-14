ed12e8dd1eb81a60c692945911e68ade0a481bed
Jesus Avila <jesusar90@hotmail.com>
2016-06-13 22:29:19 -0500

    13062016 : Creacion de DbContext, configuracion de connection string, proyectos DataAccess y Models

---
+++ WebDeveloper.DataAccess/WebContextDb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using WebDeveloper.Model;
+
+namespace WebDeveloper.DataAccess
+{
+    public class WebContextDb : DbContext
+    {
+        public WebContextDb() : base("name=WebDeveloperConnectionString")
+        {
+
+        }
+        public DbSet<Client> Clients { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ WebDeveloper.Model/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebDeveloper.Model
+{
+    public class Client
+    {
+        [Required]
+        [Display(Name = "Client ID: ")]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "First Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "This Last Name is required")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+    }
+}
