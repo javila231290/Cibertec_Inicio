eb993a2f3d32916790cd1cf115b0cb19f97df247
Jesus Avila <jesusar90@hotmail.com>
2016-06-15 21:37:49 -0500

    commit

--- WebDeveloper.DataAccess/ClientData.cs
+++ WebDeveloper.DataAccess/ClientData.cs
@@ -11,8 +11,13 @@ namespace WebDeveloper.DataAccess
     {
         
         public Client GetClientById (int? id)
-        {   
-                return GetList().Where(s => s.ID == id).FirstOrDefault();
+        {
+            //return GetList().Where(s => s.ID == id).FirstOrDefault();
+            using (var dbContext = new WebContextDb())
+            {
+                //return dbContext.Clients.FirstOrDefault(x => x.ID == id);
+                return dbContext.Clients.Find(id);
+            }
         }
     }
 }
